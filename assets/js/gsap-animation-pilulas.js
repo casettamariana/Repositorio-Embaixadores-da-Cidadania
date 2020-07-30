2ef85833b3cd18085eb96f423b6b2e4d26b2a2da
casettamariana <38991880+casettamariana@users.noreply.github.com>
2020-07-30 16:31:57 -0300

    Animações

--- assets/js/gsap-animation-pilulas.js
+++ assets/js/gsap-animation-pilulas.js
@@ -4,15 +4,15 @@ var video_3 = document.querySelector("#pilula-3-id");
 
 function displayVideo(video){
     var tl = gsap.timeline();
-    tl.set(video_1, {duration: 1, display: 'none', opacity: 0})
-        .set(video_2, {duration: 1, display: 'none', opacity: 0})
-        .set(video_3, {duration: 1, display: 'none', opacity: 0})
+    tl.set(video_1, {display: 'none', opacity: 0})
+        .set(video_2, {display: 'none', opacity: 0})
+        .set(video_3, {display: 'none', opacity: 0})
         
         if(video == 'op_1'){
-            tl.to(video_1, {display: 'block', duration: 1, opacity: 1})
+            tl.to(video_1, {display: 'block', duration: 0.5, opacity: 1})
         }else if(video == 'op_2'){
-            tl.to(video_2, {display: 'block', duration: 1, opacity: 1})
+            tl.to(video_2, {display: 'block', duration: 0.5, opacity: 1})
         }else if(video == 'op_3'){
-            tl.to(video_3, {display: 'block', duration: 1, opacity: 1})
+            tl.to(video_3, {display: 'block', duration: 0.5, opacity: 1})
         }
 }
