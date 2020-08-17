73eca329f37351d645e23e1c661ba3546e4c995c
casettamariana <38991880+casettamariana@users.noreply.github.com>
2020-08-17 12:36:35 -0300

    Versão 1.4
    Versão 1.4

--- assets/js/gsap-animation-index.js
+++ assets/js/gsap-animation-index.js
@@ -11,7 +11,7 @@ var container_pilulas = document.querySelector("#container-pilulas");
 var container_atividades = document.querySelector("#container-atividades");
 var container_podcast = document.querySelector("#container-podcast");
 var container_videocast = document.querySelector("#container-videocast");
-var container_webnar = document.querySelector("#container-webnar");
+var container_webnar = document.querySelector("#container-webinar");
 var op_outros = document.querySelector(".ul__outros");
 
 const btn_home = document.querySelector(".item1");
@@ -30,45 +30,45 @@ var controlBtn_outros = false;
 //Animação troca de conteiners
 
 
+btn_home.addEventListener('click', () => {
+    console.log('Clicou Home');
+    desapareceContainers();
+    if(controlBtn_outros){btn_outros.click();}
+    document.location.reload(true);
+});
+
 btn_pilulas.addEventListener('click', () => {
     console.log('Clicou Pilulas');
-    desapareceContainer();
-    if(controlBtn_outros){
-        btn_outros.click();
-    }
+    desapareceContainers();
+    if(controlBtn_outros){btn_outros.click();}
     container_pilulas.style.display = 'grid';
 
 });
 btn_atividades.addEventListener('click', () => {
     console.log('Clicou Atividades');
-    desapareceContainer();
-    if(controlBtn_outros){
-        btn_outros.click();
-    }
+    desapareceContainers();
+    if(controlBtn_outros){btn_outros.click();}
     container_atividades.style.display = 'flex';
 });
 btn_podcast.addEventListener('click', () => {
-    desapareceContainer();
-    if(controlBtn_outros){
-        btn_outros.click();
-    }
+    desapareceContainers();
+    if(controlBtn_outros){btn_outros.click();}
     container_podcast.style.display = 'grid';
 });
 btn_outros__item1.addEventListener('click', () => {
-    desapareceContainer();
-    if(controlBtn_outros){
-        btn_outros.click();
-    }
+    desapareceContainers();
+    if(controlBtn_outros){btn_outros.click();}
     container_videocast.style.display = 'grid';
-    document.body.style.backgroundImage  = "url('videocast.png')";
+    document.body.style.background  = "url('http://www.homolog.estudantesdeatitude.go.gov.br/repositorio_embaixadores/assets/img/backgrounds/videocast.png') no-repeat right top ";
+    document.body.style.backgroundSize = "cover"; 
 });
 btn_outros__item2.addEventListener('click', () => {
-    desapareceContainer();
-    if(controlBtn_outros){
-        container_webnar.click();
-    }
+    desapareceContainers();
+    if(controlBtn_outros){container_webnar.click();}
     container_webnar.style.display = 'grid';
-    document.body.style.backgroundImage = "url('../img/backgrounds/webnar.png')";
+    document.body.style.backgroundImage = "url('http://www.homolog.estudantesdeatitude.go.gov.br/repositorio_embaixadores/assets/img/backgrounds/webnar1920x947.png')";
+    document.body.style.backgroundSize = "cover"; 
+
 });
 btn_outros.addEventListener('click', () => {
     if(!controlBtn_outros){
@@ -81,7 +81,7 @@ btn_outros.addEventListener('click', () => {
     
 });
 
-function desapareceContainer(){
+function desapareceContainers(){
     container_podcast.style.display = "none";
     container_atividades.style.display = "none";
     container_pilulas.style.display = "none";
