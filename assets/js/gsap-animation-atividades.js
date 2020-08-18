6e70233a4b17eb504d01ebd884fd383c790d2295
casettamariana <38991880+casettamariana@users.noreply.github.com>
2020-08-18 15:47:42 -0300

    Versão 1.4.6

--- assets/js/gsap-animation-atividades.js
+++ assets/js/gsap-animation-atividades.js
@@ -1,14 +1,14 @@
-function pdf(ativ){
+function pdf(ativ) {
 
-    if(ativ == 'atividade1'){
+    if (ativ == 'atividade1') {
         window.open("./assets/pdf/Atividade1-Embaixadores_da_Cidadania.pdf", "", "");
-    } else if(ativ == 'atividade2'){
+    } else if (ativ == 'atividade2') {
         window.open("./assets/pdf/Atividade2-Embaixadores_da_Cidadania.pdf", "", "");
-    } else if(ativ == 'atividade3'){
+    } else if (ativ == 'atividade3') {
         window.open("./assets/pdf/Atividade3-Embaixadores_da_Cidadania.pdf", "", "");
-    } else if(ativ == 'atividade4'){
+    } else if (ativ == 'atividade4') {
         window.open("./assets/pdf/Atividade4-Embaixadores_da_Cidadania.pdf", "", "");
-    } else if(ativ == 'atividade5'){
+    } else if (ativ == 'atividade5') {
         window.open("./assets/pdf/Atividade5-Embaixadores_da_Cidadania.pdf", "", "");
     }
 }
--- assets/js/gsap-animation-index.js
+++ assets/js/gsap-animation-index.js
@@ -1,8 +1,14 @@
 //Animação navbar
 const btn_navbar__click = document.querySelector("#menu-btn-id");
 
-var opition_navbar = gsap.to(".slidebar", {x: '100%', duration: .5});
-var btn_navbar = gsap.to(".menu-btn", {x: '100%', duration: .6});
+var opition_navbar = gsap.to(".slidebar", {
+    x: '100%',
+    duration: .5
+});
+var btn_navbar = gsap.to(".menu-btn", {
+    x: '100%',
+    duration: .6
+});
 let controlMenu_navbar = false;
 
 //Animação de troca de páginas
@@ -33,55 +39,67 @@ var controlBtn_outros = false;
 btn_home.addEventListener('click', () => {
     console.log('Clicou Home');
     desapareceContainers();
-    if(controlBtn_outros){btn_outros.click();}
+    if (controlBtn_outros) {
+        btn_outros.click();
+    }
     document.location.reload(true);
 });
 
 btn_pilulas.addEventListener('click', () => {
     console.log('Clicou Pilulas');
     desapareceContainers();
-    if(controlBtn_outros){btn_outros.click();}
+    if (controlBtn_outros) {
+        btn_outros.click();
+    }
     container_pilulas.style.display = 'grid';
 
 });
 btn_atividades.addEventListener('click', () => {
     console.log('Clicou Atividades');
     desapareceContainers();
-    if(controlBtn_outros){btn_outros.click();}
+    if (controlBtn_outros) {
+        btn_outros.click();
+    }
     container_atividades.style.display = 'flex';
 });
 btn_podcast.addEventListener('click', () => {
     desapareceContainers();
-    if(controlBtn_outros){btn_outros.click();}
+    if (controlBtn_outros) {
+        btn_outros.click();
+    }
     container_podcast.style.display = 'grid';
 });
 btn_outros__item1.addEventListener('click', () => {
     desapareceContainers();
-    if(controlBtn_outros){btn_outros.click();}
+    if (controlBtn_outros) {
+        btn_outros.click();
+    }
     container_videocast.style.display = 'grid';
-    document.body.style.background  = "url('http://www.homolog.estudantesdeatitude.go.gov.br/repositorio_embaixadores/assets/img/backgrounds/videocast.png') no-repeat right top ";
-    document.body.style.backgroundSize = "cover"; 
+    document.body.style.background = "url('http://www.homolog.estudantesdeatitude.go.gov.br/repositorio_embaixadores/assets/img/backgrounds/videocast.png') no-repeat right top ";
+    document.body.style.backgroundSize = "cover";
 });
 btn_outros__item2.addEventListener('click', () => {
     desapareceContainers();
-    if(controlBtn_outros){container_webnar.click();}
+    if (controlBtn_outros) {
+        container_webnar.click();
+    }
     container_webnar.style.display = 'grid';
     document.body.style.backgroundImage = "url('http://www.homolog.estudantesdeatitude.go.gov.br/repositorio_embaixadores/assets/img/backgrounds/webnar1920x947.png')";
-    document.body.style.backgroundSize = "cover"; 
+    document.body.style.backgroundSize = "cover";
 
 });
 btn_outros.addEventListener('click', () => {
-    if(!controlBtn_outros){
+    if (!controlBtn_outros) {
         op_outros.style.display = 'flex';
         controlBtn_outros = true;
     } else {
         op_outros.style.display = 'none';
         controlBtn_outros = false;
     }
-    
+
 });
 
-function desapareceContainers(){
+function desapareceContainers() {
     container_podcast.style.display = "none";
     container_atividades.style.display = "none";
     container_pilulas.style.display = "none";
@@ -90,4 +108,3 @@ function desapareceContainers(){
     container_webnar.style.display = "none";
     document.body.style.background = 'transparent';
 }
-
