85afca04009e2b0ab2e0ade4028553d17dbaaf32
casettamariana <38991880+casettamariana@users.noreply.github.com>
2020-08-18 13:27:19 -0300

    Versão 1.4.1

--- assets/js/gsap-animation-pilulas.js
+++ assets/js/gsap-animation-pilulas.js
@@ -1,18 +1,21 @@
-var video_1 = document.querySelector(".pilula-1");
-var video_2 = document.querySelector(".pilula-2");
-var video_3 = document.querySelector(".pilula-3");
+var video_1 = document.querySelector("#video_pilula_1");
+var video_2 = document.querySelector("#video_pilula_2");
+var video_3 = document.querySelector("#video_pilula_3");
 
 function displayVideo(video){
     var tl = gsap.timeline();
     tl.set(video_1, {display: 'none', opacity: 0})
         .set(video_2, {display: 'none', opacity: 0})
-        .set(video_3, {display: 'none', opacity: 0})
+        .set(video_3, {display: 'none', opacity: 0});
         
         if(video == 'op_1'){
+            stopVideo();
             tl.to(video_1, {display: 'block', duration: 0.5, opacity: 1});
         }else if(video == 'op_2'){
+            stopVideo();
             tl.to(video_2, {display: 'block', duration: 0.5, opacity: 1});
         }else if(video == 'op_3'){
+            stopVideo();
             tl.to(video_3, {display: 'block', duration: 0.5, opacity: 1});
         }
 }
@@ -29,7 +32,6 @@ var vd_3 = document.getElementById('player_pilula_3');
 function btnHover(op){
     if(op == 'op_1'){
         img_op_1.src = "./assets/img/marcador/play-100px.png";
-        
     }else if(op == 'op_2'){
         img_op_2.src = "./assets/img/marcador/play-100px.png";
     }else if(op == 'op_3'){
@@ -45,7 +47,4 @@ function btnBack(op){
     }else if(op == 'op_3'){
         img_op_3.src = "./assets/img/marcador/3-100px.png";
     }
-}
-function stopVideo(video){
-    $(video)[0].contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');
 }
