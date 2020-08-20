7254cefa7429996269202d093d4d7cc8b6d3bc6e
casettamariana <38991880+casettamariana@users.noreply.github.com>
2020-08-20 14:35:37 -0300

    Responsivo podcast

--- assets/js/gsap-animation-podcast.js
+++ assets/js/gsap-animation-podcast.js
@@ -4,23 +4,41 @@ var podcast_3 = document.querySelector(".podcast_3");
 var podcast_4 = document.querySelector(".podcast_4");
 var podcast_5 = document.querySelector(".podcast_5");
 
+var op_pod_1 = document.querySelector(".opition_podcast-1");
+var op_pod_2 = document.querySelector(".opition_podcast-2");
+var op_pod_3 = document.querySelector(".opition_podcast-3");
+var op_pod_4 = document.querySelector(".opition_podcast-4");
+var op_pod_5 = document.querySelector(".opition_podcast-5");
+
 function spotifyOption(op) {
     var tl = gsap.timeline();
     tl.set(podcast_1, {display: 'none',opacity: 0})
         .set(podcast_2, {display: 'none',opacity: 0})
         .set(podcast_3, {display: 'none',opacity: 0})
         .set(podcast_4, {display: 'none',opacity: 0})
-        .set(podcast_5, {display: 'none',opacity: 0);
+        .set(podcast_5, {display: 'none',opacity: 0});
+
+        op_pod_1.classList.remove('highlight_op');
+        op_pod_2.classList.remove('highlight_op');
+        op_pod_3.classList.remove('highlight_op');
+        op_pod_4.classList.remove('highlight_op');
+        op_pod_5.classList.remove('highlight_op');
+
     if (op == 'podcast1') {
         tl.to(podcast_1, {display: 'block', duration: 0.5, opacity: 1});
+        op_pod_1.classList.add('highlight_op');
     } else if (op == 'podcast2') {
         tl.to(podcast_2, {display: 'block', duration: 0.5, opacity: 1});
+        op_pod_2.classList.add('highlight_op');
     } else if (op == 'podcast3') {
         tl.to(podcast_3, {display: 'block', duration: 0.5, opacity: 1});
+        op_pod_3.classList.add('highlight_op');
     } else if (op == 'podcast4') {
         tl.to(podcast_4, {display: 'block', duration: 0.5, opacity: 1});
+        op_pod_4.classList.add('highlight_op');
     } else if (op == 'podcast5') {
         tl.to(podcast_5, {display: 'block', duration: 0.5,opacity: 1});
+        op_pod_5.classList.add('highlight_op');
     }
 }
 
