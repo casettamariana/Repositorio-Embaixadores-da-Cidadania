eec0e94591568cd0edec73c5ec3ee92c457fe6dd
casettamariana <38991880+casettamariana@users.noreply.github.com>
2020-08-24 09:01:02 -0300

    Versão 1.5
    Nestá versão, muitos bugs como: continuação de vídeos (mesmo quando se muda de itens ou páginas), espaçamentos, mudanças visuais e operacionais foram incluídas

--- assets/js/gsap-animation-atividades.js
+++ assets/js/gsap-animation-atividades.js
@@ -3,7 +3,7 @@ function pdf(ativ) {
     if (ativ == 'atividade1') {
         window.open("./assets/pdf/Atividade1-Embaixadores_da_Cidadania.pdf", "", "");
     } else if (ativ == 'atividade2') {
-        window.open("./assets/pdf/Atividade2-Embaixadores_da_Cidadania.pdf", "", "");
+        window.open("./assets/pdf/Atividade2-Embaixadores_da_cidadania.pdf", "", "");
     } else if (ativ == 'atividade3') {
         window.open("./assets/pdf/Atividade3-Embaixadores_da_Cidadania.pdf", "", "");
     } else if (ativ == 'atividade4') {
