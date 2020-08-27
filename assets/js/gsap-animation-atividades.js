9ffe49bdd1f6f678b1c59ff5611966aa57ea95b3
casettamariana <38991880+casettamariana@users.noreply.github.com>
2020-08-27 09:07:31 -0300

    Substituir PDF

--- assets/js/gsap-animation-atividades.js
+++ assets/js/gsap-animation-atividades.js
@@ -7,7 +7,7 @@ function pdf(ativ) {
     } else if (ativ == 'atividade3') {
         window.open("./assets/pdf/Atividade3-Embaixadores_da_Cidadania.pdf", "", "");
     } else if (ativ == 'atividade4') {
-        window.open("./assets/pdf/Atividade4-Embaixadores_da_Cidadania.pdf", "", "");
+        window.open("./assets/pdf/Atividade_4_Embaixadores_da_Cidadania2.pdf", "", "");
     } else if (ativ == 'atividade5') {
         window.open("./assets/pdf/Atividade5-Embaixadores_da_Cidadania.pdf", "", "");
     }
