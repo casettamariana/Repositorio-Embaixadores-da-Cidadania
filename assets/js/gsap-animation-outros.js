10f04b3c3099a3d9ef1c21726935f7c1e0accfbf
casettamariana <38991880+casettamariana@users.noreply.github.com>
2020-09-28 11:51:23 -0300

    Troca para variáveis objeto

--- assets/js/gsap-animation-outros.js
+++ assets/js/gsap-animation-outros.js
@@ -1,208 +1,263 @@
 
 function displayVideoOutros(video) {
-    var video_1 = document.querySelector(".webnar_item1");
-    var video_2 = document.querySelector(".videocast__item1");
-    var video_3 = document.querySelector(".live__item1");
-    var video_4 = document.querySelector(".live__item2");
-    var video_5 = document.querySelector(".live__item3");
-    var video_6 = document.querySelector(".live__item4");
-    var video_7 = document.querySelector(".entrevista__item1");
-    var video_8 = document.querySelector(".oficina__item1");
-    var video_9 = document.querySelector(".oficina__item2");
-    var video_10 = document.querySelector(".tiraDuvida__item1");
-    var video_11 = document.querySelector(".tiraDuvida__item2");
-    var video_12 = document.querySelector(".outros__item1");
+
+    const video = {
+        {
+            nameClassVideo: "webnar_item1",
+            idVideo: document.querySelector(".webnar_item1"),
+            opVideo: document.querySelector(".op_webnar_outros_1"),
+            ballVideo: document.getElementById('ball_web_1')
+        },
+        {
+            nameClassVideo: "videocast__item1",
+            idVideo: document.querySelector(".videocast__item1"),
+            opVideo: document.querySelector(".op_vid_outros_1"),
+            ballVideo: document.getElementById('ball_vid_1')
+        },
+        {
+            nameClassVideo: "live__item1",
+            idVideo: document.querySelector(".live__item1"),
+            opVideo: document.querySelector(".op_live_outros_1"),
+            ballVideo: document.getElementById('ball_live_1')
+        },
+        {
+            nameClassVideo: "live__item2",
+            idVideo: document.querySelector(".live__item2"),
+            opVideo: document.querySelector(".op_live_outros_2"),
+            ballVideo: document.getElementById('ball_live_2')
+        },
+        {
+            nameClassVideo: "live__item3",
+            idVideo: document.querySelector(".live__item3"),
+            opVideo: document.querySelector(".op_live_outros_3"),
+            ballVideo: document.getElementById('ball_live_3')
+        },
+        {
+            nameClassVideo: "live__item4",
+            idVideo: document.querySelector(".live__item4"),
+            opVideo: document.querySelector(".op_live_outros_4"),
+            ballVideo: document.getElementById('ball_live_4')
+        },
+        {
+            nameClassVideo: "entrevista__item1",
+            idVideo: document.querySelector(".entrevista__item1"),
+            opVideo: document.querySelector(".op_entrevista_outros_1"),
+            ballVideo: document.getElementById('ball_entrevista_1')
+        },
+        {
+            nameClassVideo: "oficina__item2",
+            idVideo: document.querySelector(".oficina__item2"),
+            opVideo: document.querySelector(".op_oficina_outros_2"),
+            ballVideo: document.getElementById('ball_oficina_2')
+        },
+        {
+            nameClassVideo: "tiraDuvida__item1",
+            idVideo: document.querySelector(".tiraDuvida__item1"),
+            opVideo: document.querySelector(".op_tiraDuvida_outros_1"),
+            ballVideo: document.getElementById('ball_tiraDuvida_1')
+        },
+        {
+            nameClassVideo: "tiraDuvida__item2",
+            idVideo: document.querySelector(".tiraDuvida__item2"),
+            opVideo: document.querySelector(".op_tiraDuvida_outros_2"),
+            ballVideo: document.getElementById('ball_tiraDuvida_2')
+        },
+        {
+            nameClassVideo: "outros__item1",
+            idVideo: document.querySelector(".outros__item1"),
+            opVideo: document.querySelector(".op_outros_outros_1"),
+            ballVideo: document.getElementById('ball_outros_1')
+        }
+        };
     
-    var div_op_1 = document.querySelector(".op_webnar_outros_1");
-    var div_op_2 = document.querySelector(".op_vid_outros_1");
-    var div_op_3 = document.querySelector(".op_live_outros_1");
-    var div_op_4 = document.querySelector(".op_live_outros_2");
-    var div_op_5 = document.querySelector(".op_live_outros_3");
-    var div_op_6 = document.querySelector(".op_live_outros_4");
-    var div_op_7 = document.querySelector(".op_entrevista_outros_1");
-    var div_op_8 = document.querySelector(".op_oficina_outros_1");
-    var div_op_9 = document.querySelector(".op_oficina_outros_2");
-    var div_op_10 = document.querySelector(".op_tiraDuvida_outros_1");
-    var div_op_11 = document.querySelector(".op_tiraDuvida_outros_2");
-    var div_op_12 = document.querySelector(".op_outros_outros_1");
+    
 
     var titulo = document.querySelector('.title_videos_outros');
 
     var tl = gsap.timeline();
 
-    
-    tl.set(video_1, {
-            display: 'none',
-            opacity: 0
-        })
-        .set(video_2, {
-            display: 'none',
-            opacity: 0
-        })
-        .set(video_3, {
-            display: 'none',
-            opacity: 0
-        })
-        .set(video_4, {
-            display: 'none',
-            opacity: 0
-        })
-        .set(video_5, {
-            display: 'none',
-            opacity: 0
-        })
-        .set(video_6, {
-            display: 'none',
-            opacity: 0
-        })
-        .set(video_7, {
-            display: 'none',
-            opacity: 0
-        })
-        .set(video_8, {
-            display: 'none',
-            opacity: 0
-        })
-        .set(video_9, {
-            display: 'none',
-            opacity: 0
-        })
-        .set(video_10, {
-            display: 'none',
-            opacity: 0
-        })
-        .set(video_11, {
-            display: 'none',
-            opacity: 0
-        })
-        .set(video_12, {
-            display: 'none',
-            opacity: 0
-        })
-        ;
+    var myMap = new Map();
 
-        div_op_1.classList.remove('highlight_op_outros');
-        div_op_2.classList.remove('highlight_op_outros');
-        div_op_3.classList.remove('highlight_op_outros');
-        div_op_4.classList.remove('highlight_op_outros');
-        div_op_5.classList.remove('highlight_op_outros');
-        div_op_6.classList.remove('highlight_op_outros');
-        div_op_7.classList.remove('highlight_op_outros');
-        div_op_8.classList.remove('highlight_op_outros');
-        div_op_9.classList.remove('highlight_op_outros');
-        div_op_10.classList.remove('highlight_op_outros');
-        div_op_11.classList.remove('highlight_op_outros');
-        div_op_12.classList.remove('highlight_op_outros');
+    myMap.set(video, {function() {
+        tl.set(video.idVideo, {
+            display: 'none',
+            opacity: 0
+        });
+        video.idVideo.classList.remove('highlight_op_outros')
+    }});
+
+    // tl.set(video_1, {
+    //         display: 'none',
+    //         opacity: 0
+    //     })
+    //     .set(video_2, {
+    //         display: 'none',
+    //         opacity: 0
+    //     })
+    //     .set(video_3, {
+    //         display: 'none',
+    //         opacity: 0
+    //     })
+    //     .set(video_4, {
+    //         display: 'none',
+    //         opacity: 0
+    //     })
+    //     .set(video_5, {
+    //         display: 'none',
+    //         opacity: 0
+    //     })
+    //     .set(video_6, {
+    //         display: 'none',
+    //         opacity: 0
+    //     })
+    //     .set(video_7, {
+    //         display: 'none',
+    //         opacity: 0
+    //     })
+    //     .set(video_8, {
+    //         display: 'none',
+    //         opacity: 0
+    //     })
+    //     .set(video_9, {
+    //         display: 'none',
+    //         opacity: 0
+    //     })
+    //     .set(video_10, {
+    //         display: 'none',
+    //         opacity: 0
+    //     })
+    //     .set(video_11, {
+    //         display: 'none',
+    //         opacity: 0
+    //     })
+    //     .set(video_12, {
+    //         display: 'none',
+    //         opacity: 0
+    //     })
+    //     ;
+
+        // div_op_1.classList.remove('highlight_op_outros');
+        // div_op_2.classList.remove('highlight_op_outros');
+        // div_op_3.classList.remove('highlight_op_outros');
+        // div_op_4.classList.remove('highlight_op_outros');
+        // div_op_5.classList.remove('highlight_op_outros');
+        // div_op_6.classList.remove('highlight_op_outros');
+        // div_op_7.classList.remove('highlight_op_outros');
+        // div_op_8.classList.remove('highlight_op_outros');
+        // div_op_9.classList.remove('highlight_op_outros');
+        // div_op_10.classList.remove('highlight_op_outros');
+        // div_op_11.classList.remove('highlight_op_outros');
+        // div_op_12.classList.remove('highlight_op_outros');
         
 
-
-    if (video == 'op_web_1') {
-        tl.to(video_1, {
-            display: 'flex',
-            duration: 0.5,
-            opacity: 1
-        });
-        div_op_1.classList.add('highlight_op_outros');
-        titulo.innerHTML = "WEBINAR";
-    } else if (video == 'op_videocast_1') {
-        tl.to(video_2, {
-            display: 'flex',
-            duration: 0.5,
-            opacity: 1
-        });
-        div_op_2.classList.add('highlight_op_outros');
-        titulo.innerHTML = "VIDEOCAST";
-    } else if (video == 'op_live_1') {
-        tl.to(video_3, {
-            display: 'flex',
-            duration: 0.5,
-            opacity: 1
-        });
-        div_op_3.classList.add('highlight_op_outros');
-        titulo.innerHTML = "LIVE FACEBOOK";
+        
+        // if (video == 'op_web_1') {
+        //     tl.to(video_1, {
+        //         display: 'flex',
+        //         duration: 0.5,
+        //         opacity: 1
+        //     });
+        //     div_op_1.classList.add('highlight_op_outros');
+        //     titulo.innerHTML = "WEBINAR";
+        // } else if (video == 'op_videocast_1') {
+        //     tl.to(video_2, {
+        //         display: 'flex',
+        //         duration: 0.5,
+        //         opacity: 1
+        //     });
+        //     div_op_2.classList.add('highlight_op_outros');
+        //     titulo.innerHTML = "VIDEOCAST";
+        // } else if (video == 'op_live_1') {
+        //     tl.to(video_3, {
+        //         display: 'flex',
+        //         duration: 0.5,
+        //         opacity: 1
+        //     });
+        //     div_op_3.classList.add('highlight_op_outros');
+        //     titulo.innerHTML = "LIVE FACEBOOK";
+        // }
+        // else if (video == 'op_live_2') {
+        //     tl.to(video_4, {
+        //         display: 'flex',
+        //         duration: 0.5,
+        //         opacity: 1
+        //     });
+        //     div_op_4.classList.add('highlight_op_outros');
+        //     titulo.innerHTML = "LIVE FACEBOOK";
+        // }
+        // else if (video == 'op_live_3') {
+        //     tl.to(video_5, {
+        //         display: 'flex',
+        //         duration: 0.5,
+        //         opacity: 1
+        //     });
+        //     div_op_5.classList.add('highlight_op_outros');
+        //     titulo.innerHTML = "LIVE FACEBOOK";
+        // }
+        // else if (video == 'op_live_4') {
+        //     tl.to(video_6, {
+        //         display: 'flex',
+        //         duration: 0.5,
+        //         opacity: 1
+        //     });
+        //     div_op_6.classList.add('highlight_op_outros');
+        //     titulo.innerHTML = "LIVE YOUTUBE";
+        // }
+        // else if (video == 'op_entrevista_1') {
+        //     tl.to(video_7, {
+        //         display: 'flex',
+        //         duration: 0.5,
+        //         opacity: 1
+        //     });
+        //     div_op_7.classList.add('highlight_op_outros');
+        //     titulo.innerHTML = "ENTREVISTAS";
+        // }
+        // else if (video == 'op_oficina_1') {
+        //     tl.to(video_8, {
+        //         display: 'flex',
+        //         duration: 0.5,
+        //         opacity: 1
+        //     });
+        //     div_op_8.classList.add('highlight_op_outros');
+        //     titulo.innerHTML = "OFICINAS";
+        // }
+        // else if (video == 'op_oficina_2') {
+        //     tl.to(video_9, {
+        //         display: 'flex',
+        //         duration: 0.5,
+        //         opacity: 1
+        //     });
+        //     div_op_9.classList.add('highlight_op_outros');
+        //     titulo.innerHTML = "OFICINAS";
+        // }
+        // else if (video == 'op_tiraDuvida_1') {
+        //     tl.to(video_10, {
+        //         display: 'flex',
+        //         duration: 0.5,
+        //         opacity: 1
+        //     });
+        //     div_op_10.classList.add('highlight_op_outros');
+        //     titulo.innerHTML = "Evento Tira Duvidas";
+        // }
+        // else if (video == 'op_tiraDuvida_2') {
+        //     tl.to(video_11, {
+        //         display: 'flex',
+        //         duration: 0.5,
+        //         opacity: 1
+        //     });
+        //     div_op_11.classList.add('highlight_op_outros');
+        //     titulo.innerHTML = "Evento Tira Duvidas";
+        // }
+        // else if (video == 'op_outros_1') {
+        //     tl.to(video_12, {
+        //         display: 'flex',
+        //         duration: 0.5,
+        //         opacity: 1
+        //     });
+        //     div_op_12.classList.add('highlight_op_outros');
+        //     titulo.innerHTML = "Outros";
+        // }
     }
-    else if (video == 'op_live_2') {
-        tl.to(video_4, {
-            display: 'flex',
-            duration: 0.5,
-            opacity: 1
-        });
-        div_op_4.classList.add('highlight_op_outros');
-        titulo.innerHTML = "LIVE FACEBOOK";
-    }
-    else if (video == 'op_live_3') {
-        tl.to(video_5, {
-            display: 'flex',
-            duration: 0.5,
-            opacity: 1
-        });
-        div_op_5.classList.add('highlight_op_outros');
-        titulo.innerHTML = "LIVE FACEBOOK";
-    }
-    else if (video == 'op_live_4') {
-        tl.to(video_6, {
-            display: 'flex',
-            duration: 0.5,
-            opacity: 1
-        });
-        div_op_6.classList.add('highlight_op_outros');
-        titulo.innerHTML = "LIVE YOUTUBE";
-    }
-    else if (video == 'op_entrevista_1') {
-        tl.to(video_7, {
-            display: 'flex',
-            duration: 0.5,
-            opacity: 1
-        });
-        div_op_7.classList.add('highlight_op_outros');
-        titulo.innerHTML = "ENTREVISTAS";
-    }
-    else if (video == 'op_oficina_1') {
-        tl.to(video_8, {
-            display: 'flex',
-            duration: 0.5,
-            opacity: 1
-        });
-        div_op_8.classList.add('highlight_op_outros');
-        titulo.innerHTML = "OFICINAS";
-    }
-    else if (video == 'op_oficina_2') {
-        tl.to(video_9, {
-            display: 'flex',
-            duration: 0.5,
-            opacity: 1
-        });
-        div_op_9.classList.add('highlight_op_outros');
-        titulo.innerHTML = "OFICINAS";
-    }
-    else if (video == 'op_tiraDuvida_1') {
-        tl.to(video_10, {
-            display: 'flex',
-            duration: 0.5,
-            opacity: 1
-        });
-        div_op_10.classList.add('highlight_op_outros');
-        titulo.innerHTML = "Evento Tira Duvidas";
-    }
-    else if (video == 'op_tiraDuvida_2') {
-        tl.to(video_11, {
-            display: 'flex',
-            duration: 0.5,
-            opacity: 1
-        });
-        div_op_11.classList.add('highlight_op_outros');
-        titulo.innerHTML = "Evento Tira Duvidas";
-    }
-    else if (video == 'op_outros_1') {
-        tl.to(video_12, {
-            display: 'flex',
-            duration: 0.5,
-            opacity: 1
-        });
-        div_op_12.classList.add('highlight_op_outros');
-        titulo.innerHTML = "Outros";
-    }
-}
 
 
 
